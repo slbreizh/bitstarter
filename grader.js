1696a76639ca4934b07d13bd870b5c66da987a85
slbreizh <me@slbreizh.me>
2013-07-08 11:31:54 +0800

    grader with url

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+/*#!/usr/bin/env node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = 'https://bitstarter-123.herokuapp.com';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +39,29 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+var checkURL = function(url, checksFile){
+  rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log("Error while accessing the page %s.", url);
+      process.exit(1);
+    } else {
+      var checkJson = checkHtmlStr(result, checksFile);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+  });
+}
+
+var cheerioString = function(htmlStr){
+  return cheerio.load(htmlStr);
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlStr = function(htmlStr, checksfile) {
+    $ = cheerioString(htmlStr);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -53,6 +69,10 @@ var checkHtmlFile = function(htmlfile, checksfile) {
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    return checkHtmlStr(fs.readFileSync(htmlfile), checksfile);
 };
 
 var clone = function(fn) {
@@ -65,10 +85,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of the page to grade')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        
+    if (program.url){
+      checkURL(program.url, program.checks);
+    }
+    else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
